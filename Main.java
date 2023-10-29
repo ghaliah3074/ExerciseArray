900268e66ffddccc1f1912a35ad1671b641192a9
ghaliah3074 <148686086+ghaliah3074@users.noreply.github.com>
2023-10-29 19:57:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+//        int Array0[] = {50, -20, 0, 30, 40, 60, 10};
+//        if (Array0.length >= 2 && Array0[0] == Array0.length - 1) {
+//
+//
+//            System.out.println("tru");
+//        } else {
+//            System.out.println("false");
+//        }
+
+
+        //q2
+//        int Array11[] = {1, 4, 17, 7, 25, 3, 100};
+//        int sum=0;
+//        System.out.println(Arrays.toString(Array11));
+//        for (int i=0;i<Array11.length;i++){
+//            sum =sum +Array11[i];
+//        }
+//        double average =sum/Array11.length;
+//        System.out.println("The average of the sid Array  is :   "+average);
+//        System.out.println("the number in the sid Arraythat are  greater than the average are :  ");
+//        for (int i = 0; i < Array11.length; i++) {
+//            if ( Array11[i]>average){
+//                System.out.println(Array11[i]);
+//            }
+//        }
+//
+
+        //q3
+
+//        int Array2[] = {20, 30, 40};
+//
+//        int num=Array2[0];
+//        if (Array2[2]>=num){
+//            num=Array2[2];
+//            System.out.println(" larger value between first and last element: "+num);
+//        }
+
+
+        //q4
+//        ArrayList<Integer> Array6=new ArrayList<Integer>();
+//        Array6.add(20);
+//        Array6.add(30);
+//        Array6.add(40);
+//        Array6.set(0,40);
+//        Array6.set(2,20);
+//
+//        System.out.println(Array6);
+
+
+
+
+
+
+
+        //q5
+//        int Array3[] = {2, 3, 40, 1, 5, 9, 4, 10, 7};
+//        System.out.println(" Original Array :  " + Arrays.toString(Array3));
+//        int i = 0;
+//        while (i < Array3.length && Array3[i] % 2 == 0)
+//            i++;
+//
+//        for (int j = 0; j < Array3.length; j++) {
+//
+//            if (Array3[j] % 2 != 0) {
+//                int a_num = Array3[i];
+//                Array3[i] = Array3[j];
+//                Array3[j] = a_num;
+//                i++;
+//            }
+//        }
+//        System.out.println("New Array: " + Arrays.toString(Array3));
+
+
+
+        //q6
+
+
+//        int Array4[]={2,3,6,6,4};
+//        int Array5[]={2,3,6,6,4,};
+//        for (int i = 0; i < Array4.length; i++) {
+//
+//        }
+//        if (Array4.length==Array5.length){
+//            System.out.println("true");
+//        }
+//else {
+//            System.out.println("false");
+//        }
+
+
+
+    }
+}
